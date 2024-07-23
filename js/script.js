5cd160134381dea77d73f78ed5310caf07466005
Victor Reyes Galvez <54037366+tenshi98@users.noreply.github.com>
2024-07-23 09:48:16 -0400

    Actualizacion general

--- js/script.js
+++ js/script.js
@@ -59,36 +59,7 @@ if (currentTheme) {
   }
 }
 
-document.getElementById('toggle-button_1').addEventListener('click', function () {
-  toggle(document.querySelectorAll('.card_hide'));
-});
-document.getElementById('toggle-button_2').addEventListener('click', function () {
-  toggle(document.querySelectorAll('.job_hide'));
-});
 
-//toggle function
-function toggle (elements, specifiedDisplay) {
-  var element, index;
-
-  elements = elements.length ? elements : [elements];
-  for (index = 0; index < elements.length; index++) {
-    element = elements[index];
-
-    if (isElementHidden(element)) {
-      element.style.display = '';
-
-      // If the element is still hidden after removing the inline display
-      if (isElementHidden(element)) {
-        element.style.display = specifiedDisplay || 'block';
-      }
-    } else {
-      element.style.display = 'none';
-    }
-  }
-  function isElementHidden (element) {
-    return window.getComputedStyle(element, null).getPropertyValue('display') === 'none';
-  }
-}
 
 
 
